f67e3e046fb4767353492ce016c369f0aed9a86b
Kunal Mehrotra <kunalmehrotra@Kunals-MacBook-Pro.local>
2022-11-19 03:46:00 -0500

    structue

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,11 +1,36 @@
 import posts from "./tuits.js";
 let tuits = posts;
 
+const defaults = {
+  postedBy: {
+    username: "NASA",
+  },
+  verified: false,
+  handle: "@nasa",
+  tuit: "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+  logoImage: "/tuiter/images/elon_musk.jpeg",
+  avatarImage: "/tuiter/images/elon_musk.jpeg",
+  tuits: "122K",
+  stats: {
+    comments: 123,
+    retuits: 432,
+    likes: 2345,
+  },
+  topic: "Space",
+  time: "2h",
+  liked: false,
+  replies: 0,
+  retuits: 0,
+  likes: 0,
+};
+
 const createTuit = (req, res) => {
-  const newTuit = req.body;
-  newTuit._id = new Date().getTime() + "";
-  newTuit.likes = 0;
-  newTuit.liked = false;
+  const newTuit = {
+    ...req.body,
+    ...defaults,
+    _id: new Date().getTime() + "",
+    liked: false,
+  };
   tuits.push(newTuit);
   res.json(newTuit);
 };
